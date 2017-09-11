42b0174bf57edf09aef8849d266462dbb5ec5736
akshayantony <akshay55.sayone@gmail.com>
2017-09-11 17:37:40 +0530

    1

--- homepage/models.py
+++ homepage/models.py
@@ -11,7 +11,7 @@ from django.http import HttpResponse
 from django.dispatch import receiver
 
 class NewsCategories(models.Model):
-    cname = models.CharField(max_length=100)
+    cname = models.CharField(max_length=100,primary_key=True)
 
     def __str__(self):
         return self.cname
