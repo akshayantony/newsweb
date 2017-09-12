6c76b387f3de7886702e0d7cecea2e1deeaf362a
akshayantony <akshay55.sayone@gmail.com>
2017-09-12 14:34:09 +0530

    upto passwordreset

--- newsweb/urls.py
+++ newsweb/urls.py
@@ -26,10 +26,10 @@ urlpatterns = [
     url(r'^accounts/login/$',auth_views.login,{'template_name':'signup/login.html'},name='login1'),
     url(r'^logout/$',auth_views.logout,{'next_page':'homepage:news_list'},name='logout'),
 
-    url(r'^password_reset/$',auth_views.password_reset,name='password_reset'),
-    url(r'^password_reset/done/$',auth_views.password_reset_done,name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm,name="password_reset_confirm"),
-    url(r'^reset/done/$',auth_views.password_reset_complete,name='password_reset_complete'),
+    url(r'^password_reset/$',auth_views.password_reset,{'template_name':'signup/password_reset_form.html'},name='password_reset'),
+    url(r'^password_reset/done/$',auth_views.password_reset_done,{'template_name':'signup/password_reset_done.html'},name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm,{'template_name':'signup/password_reset_confirm.html'},name="password_reset_confirm"),
+    url(r'^reset/done/$',auth_views.password_reset_complete,{'template_name':'signup/password_reset_complete'},name='password_reset_complete'),
 
     url(r'^password/$',core_views.change_password,name='change_password'),
 
